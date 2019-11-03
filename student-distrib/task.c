2b8eb10bfc713403944e345a008068c96fe1e628
zikail2 <zikail2@illinois.edu>
2019-11-02 23:14:42 -0500

    Halfdone execute.

--- student-distrib/task.c
+++ student-distrib/task.c
@@ -2,3 +2,68 @@
 // Created by liuzikai on 11/2/19.
 //
 
+#include "task.h"
+#include "lib.h"
+
+uint32_t process_cnt = 0;
+process_t* cur_process = NULL;
+
+static const uint8_t* empty_args = "";
+
+process_t* process_create() {
+
+    if (process_cnt >= PROCESS_MAX_CNT) return NULL;
+
+    process_t* proc = ptr_process(process_cnt++);  // allocate a new PCB
+
+    proc->opened_files = ;  // init opened file list
+    proc->parent = cur_process;
+    proc->kesp = ((uint32_t) proc) + PKM_SIZE_IN_BYTES;  // initialize kernel esp to the bottom of PKM
+
+    return proc;
+}
+
+void process_remove() {
+
+}
+
+int32_t execute_parse_command(uint8_t* command, uint8_t** args) {
+    while (*command != '\0') {
+        if (*command == ' ') {
+            *command = '\0';  // split executable name and args;
+            *args = command + 1;  // output args string
+            return 0;
+        }
+        command++;  // move to next character
+    }
+    // No args
+    *args = empty_args;
+    return 0;
+}
+
+int32_t system_execute(uint8_t *command) {
+
+    process_t* proc = process_create();
+    uint32_t start_eip;
+    int32_t program_ret;
+
+    if (proc == NULL) return -1;  // failed to create new process
+
+    if (execute_parse_command(command, &proc->args) != 0) return -1;  // invalid command
+
+    if ((start_eip = /* TODO: open executable file */) == 0) return -1;  // invalid executable file
+
+    execute_launch(USER_STACK_STARTING_ADDR, start_eip, program_ret);
+
+    return program_ret;
+}
+
+int32_t system_halt(uint8_t status) {
+
+
+
+    printf(PRINT_ERR "halt() should never return");
+
+    return -1;
+}
+
--- student-distrib/task.h
+++ student-distrib/task.h
@@ -5,21 +5,55 @@
 #ifndef _TASK_H
 #define _TASK_H
 
+#include "types.h"
+#include "x86_desc.h"
 #include "file_system.h"
 
-// Task control block (TCB) stores info for every task
-typedef struct {
+// Process control block (PCB) stores info for every task
+typedef struct process_t process_t;
+struct process_t {
+    process_t* parent;
+    uint8_t* args;
+    uint32_t kesp;  // kernel stack esp
     file_array_t opened_files;
-} task_control_block_t;
+};
 
 
-// Task kernel memory (TKM) consists of task control block and the kernel stack for a task
+// Process kernel memory (PKM) consists of task control block and the kernel stack for a task
+#define PKM_SIZE_IN_BYTES    8192
+typedef union process_kernel_memory_t process_kernel_memory_t;
+union process_kernel_memory_t {
+    process_t tcb;
+    uint8_t kstack[PKM_SIZE_IN_BYTES];  // stack for kernel status
+};
 
-#define TKM_SIZE_IN_BYTES    8192
+#define PKM_STARTING_ADDR    0x800000  // PKM starts at 8MB (bottom of kernel image), going to low address
 
-typedef struct {
-    task_control_block_t tcb;
-    uint8_t kstack[TKM_SIZE_IN_BYTES - sizeof(task_control_block_t)];  // stack for kernel status
-} task_kernel_memory_t;
 
-#endif //_TASK_H
+#define ptr_process(idx) (((process_t *) PKM_STARTING_ADDR) - (idx + 1))  // address of idx-th process control block
+#define PROCESS_MAX_CNT    2  // maximum number of processes running at the same time
+extern uint32_t process_cnt;
+
+extern process_t* cur_process;  // current process
+
+#define USER_STACK_STARTING_ADDR  0x8400000  // User stack starts at 132MB (with paging)
+
+
+
+#define execute_launch(esp, eip, ret) asm (" \
+    pushl $1f       /* return address to label 1, on top of the stack after iret */ \n\
+    pushl $USER_DS  /* user SS  */ \n\
+    pushl %1        /* user ESP */ \n\
+    pushf           /* flags (new program should not care) */ \n\
+    pushl $USER_CS  /* user CS  */ \n\
+    pushl %2        /* user EIP */ \n\
+    iret            /* enter user program */ \n\
+1:  movl %%eax, %0" /* return value pass by halt in EAX*/ \
+    : "=rm" (ret) \
+    : "rm" (esp), "rm" (eip) \
+    : "memory" \
+)
+
+
+
+#endif // _TASK_H
