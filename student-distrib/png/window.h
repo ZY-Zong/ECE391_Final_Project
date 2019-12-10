bd1c10380ccebd158129b3b7129955ab7f3851b4
zhenyuz2 <zhenyuz2@illinois.edu>
2019-12-09 21:38:54 -0600

    PNG loading success.

--- student-distrib/png/window.h
+++ student-distrib/png/window.h
@@ -5,4 +5,15 @@
 #ifndef _WINDOW_H
 #define _WINDOW_H
 
+#include "png.h"
+#include "../vga/vga.h"
+#include "../file_system.h"
+
+void draw_png(const uint8_t *fname, int x_offset, int y_offset);
+void full_screen_png();
+void terminal_status_bar();
+void draw_pressed_yellow_button();
+void draw_pressed_green_button();
+
+
 #endif //_WINDOW_H
