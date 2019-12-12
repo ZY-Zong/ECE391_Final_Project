9f13996f4996fa50914903affa7fdaf777491a47
Aelita <tingkai.17@intl.zju.edu.cn>
2019-12-11 19:40:26 -0600

    beep:test

--- student-distrib/beep.c
+++ student-distrib/beep.c
@@ -1,6 +1,7 @@
 
 #include "beep.h"
 #include "lib.h"
+#include "rtc.h"
 
 // reference: https://wiki.osdev.org/PC_Speaker
 /* down from here */
@@ -39,6 +40,10 @@ void nosound() {
  */
 void beep(uint32_t frequency, uint32_t time_in_ms){
     play_sound(frequency);
+    
     // TODO: keep the beep for time_in_ms by sleeping 
+    // for test: just use current rtc frequency
+    system_rtc_read(0,0,0);
+
     nosound();
 }
