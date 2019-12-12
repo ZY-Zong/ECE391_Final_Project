3350c8345ba2a65443ca4d96ef631ed75fcb4eb1
Aelita <tingkai.17@intl.zju.edu.cn>
2019-12-11 21:16:57 -0600

    beep:test

--- student-distrib/beep.c
+++ student-distrib/beep.c
@@ -10,24 +10,34 @@
 void play_sound(uint32_t nFrequence) {
  	uint32_t Div;
  	uint8_t tmp;
+    uint32_t flag;
  
-    //Set the PIT to the desired frequency
- 	Div = 1193180 / nFrequence;
- 	outb(0xb6, 0x43);
- 	outb((uint8_t) (Div), 0x42);
- 	outb((uint8_t) (Div >> 8), 0x42);
+    cli_and_save(flag); {
+        //Set the PIT to the desired frequency
+ 	    Div = 1193180 / nFrequence;
+ 	    outb(0xb6, 0x43);
+ 	    outb((uint8_t) (Div), 0x42);
+ 	    outb((uint8_t) (Div >> 8), 0x42);
  
-    //And play the sound using the PC speaker
- 	tmp = inb(0x61);
-  	if (tmp != (tmp | 3)) {
- 		outb(tmp | 3, 0x61);
- 	}
+        //And play the sound using the PC speaker
+ 	    tmp = inb(0x61);
+  	    if (tmp != (tmp | 3)) {
+ 	    	outb(tmp | 3, 0x61);
+ 	    }
+    }
+    restore_flags(flag);
  }
  
  //make it shutup
 void nosound() {
- 	uint8_t tmp = inb(0x61) & 0xFC;
- 	outb(tmp, 0x61);
+ 	uint32_t flag;
+    
+    cli_and_save(flag); {
+        uint8_t tmp = inb(0x61) & 0xFC;
+ 	    outb(tmp, 0x61);
+    }
+    restore_flags(flag);
+    
  }
 
 // reference: https://wiki.osdev.org/PC_Speaker
